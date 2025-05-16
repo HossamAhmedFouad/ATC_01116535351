d424e1a11338246e3ac945616ce7f49b25ff213b
Hossam Ahmed Fouad <57450146+HossamAhmedFouad@users.noreply.github.com>
2025-05-16 04:42:26 +0300

    fix: Remove dynamic assignment of API URL in production environment

--- event-booking-app/src/environments/environment.prod.ts
+++ event-booking-app/src/environments/environment.prod.ts
@@ -14,7 +14,7 @@ declare global {
 
 export const environment = {
   production: true,
-  apiUrl: window?.env?.API_URL || 'http://172.161.48.59:3000/api',
+  apiUrl: 'http://172.161.48.59:3000/api',
   supabase: {
     url: window?.env?.SUPABASE_URL || '',
     anonKey: window?.env?.SUPABASE_ANON_KEY || '',
